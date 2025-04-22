e8c4ac0797e3bbc07e08d322f597d99d3bf2e90c
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2025-04-22 12:15:03 +0200

    fix(backend): use HabitCreateDTO in repository

--- backend/src/modules/habit/repository/habit.repository.interface.ts
+++ backend/src/modules/habit/repository/habit.repository.interface.ts
@@ -1,8 +1,8 @@
-import { Habit } from "../habit.model";
+import { Habit, HabitCreateDto } from "../habit.model";
 
 export interface HabitRepository {
   getAll(): Promise<Habit[]>;
-  create(habit: Habit): Promise<Habit>;
+  create(habit: HabitCreateDto): Promise<Habit>;
   delete(id: string): Promise<void>;
   toggleCompleteForToday(id: string): Promise<Habit>;
 }
